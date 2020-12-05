5d5dc4fa50487f2f31040a029399ac2f747cc00e
Marcus Hultman <marcus.hultman1@gmail.com>
2020-12-05 15:59:03 +0100

    5: parse as binary

--- 5/1.ts
+++ 5/1.ts
@@ -1,9 +1,6 @@
 import inputStrings from "../util/load.ts";
 
-export function bin(s: string, c: string, n: number): number {
-  return s.length ? (s[0] === c ? 0 : n / 2 ) + bin(s.slice(1), c, n / 2) : 0;
-}
-export const seatId = (s: string) => bin(s.slice(0, 7), 'F', 128) * 8 + bin(s.slice(7), 'L', 8);
+export const seatId = (s: string) => parseInt([...s].map(c => 'FBLR'.indexOf(c) & 1).join(''), 2);
 
 if (import.meta.main) {
   const lines = await inputStrings(5);
