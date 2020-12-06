8acc7385cf47f8e1ea95466f9facd6330cd4144d
Marcus Hultman <marcus.hultman1@gmail.com>
2020-12-06 22:24:24 +0100

    6: simplify a little bit

--- 6/1.ts
+++ 6/1.ts
@@ -2,6 +2,6 @@ import puzzle from '../util/puzzle.ts';
 
 const sections = await puzzle(import.meta).sections();
 
-const sectionCount = (s: string) => new Set(s.split('\n').map(s => s.split('')).flat()).size;
+const sectionCount = (s: string) => new Set(s.replace(/\n/g, '')).size;
 
 console.log(sections.map(sectionCount).reduce((a, b) => a + b, 0));
--- 6/2.ts
+++ 6/2.ts
@@ -3,6 +3,6 @@ import { intersection } from '../util/set.ts';
 
 const sections = await puzzle(import.meta).sections();
 
-const sectionCount = (s: string) => s.split('\n').map(s => new Set([...s])).reduce(intersection).size;
+const sectionCount = (s: string) => s.split('\n').map(s => new Set(s)).reduce(intersection).size;
 
 console.log(sections.map(sectionCount).reduce((a, b) => a + b, 0));
