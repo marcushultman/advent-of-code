14f607a380619a1746c37713016ac7cfbf030fc3
Marcus Hultman <marcus.hultman1@gmail.com>
2020-12-07 19:39:45 +0100

    7

---
+++ 7/1.ts
@@ -0,0 +1,21 @@
+import puzzle from '../util/puzzle.ts';
+
+const lines = await puzzle(import.meta).strings();
+
+const tree = {} as { [bag: string]: string[] };
+
+function go(bag: string): boolean {
+  return bag === 'shiny gold' || tree[bag].some(go);
+}
+
+if (import.meta.main) {
+  lines.forEach(line => {
+    const [_, bag, match] = line.match(/(.*) bags contain (.*)./) as string[];
+    tree[bag] = [];
+    if (match !== 'no other bags') {
+      tree[bag] = [...match.matchAll(/\d+ (.*?) bags?/g)].map(([a, b]) => b);
+    }
+  });
+  const found = Object.keys(tree).reduce((set, bag) => go(bag) ? set.add(bag) : set, new Set());
+  console.log(found.size - 1);
+}
