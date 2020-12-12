c4977d486953cb3f5d278541bc858e70809a05fb
Marcus Hultman <marcus.hultman1@gmail.com>
2020-12-12 13:59:43 +0100

    util: add vector & matrix

---
+++ util/matrix.ts
@@ -0,0 +1,25 @@
+import { vec2 } from './vector.ts';
+
+function toRad(deg: number) {
+  return deg * (Math.PI / 180);
+}
+
+export class mat2 {
+  constructor(
+    private x0y0: number,
+    private x1y0: number,
+    private x0y1: number,
+    private x1y1: number) {}
+  
+  static fromRotation(deg: number) {
+    const r = toRad(deg);
+    return new mat2(Math.cos(r), -Math.sin(r), Math.sin(r), Math.cos(r));
+  }
+  
+  rotate(v: vec2): vec2 {
+    return new vec2(
+      Math.round(this.x0y0 * v.x + this.x1y0 * v.y),
+      Math.round(this.x0y1 * v.x + this.x1y1 * v.y)
+    );
+  }
+}
---
+++ util/vector.ts
@@ -0,0 +1,18 @@
+
+export class vec2 {
+  constructor(public x: number, public y: number) {}
+  
+  add(v: vec2): vec2 {
+    return new vec2(this.x + v.x, this.y + v.y);
+  }
+  sub(v: vec2): vec2 {
+    return new vec2(this.x - v.x, this.y - v.y);
+  }
+  norm(): vec2 {
+    const l = Math.sqrt(this.x * this.x + this.y * this.y);
+    return new vec2(this.x / l, this.y / l);
+  }
+  mul(d: number): vec2 {
+    return new vec2(this.x * d, this.y * d);
+  }
+}
