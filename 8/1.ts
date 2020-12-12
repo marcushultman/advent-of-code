ae3a4869078f0ceec083945bb9b208e87423c86c
Marcus Hultman <marcus.hultman1@gmail.com>
2020-12-12 13:43:38 +0100

    8

---
+++ 8/1.ts
@@ -0,0 +1,22 @@
+import puzzle from '../util/puzzle.ts';
+
+const ops = await puzzle(import.meta, false).map(l => {
+  return { op : l.substr(0, 3), val: (l[4] === '-' ? -1 : 1) * Number(l.substr(5)) };
+})
+
+if (import.meta.main) {
+  let acc = 0;
+  let pos = 0;
+  const seen = new Set([pos]);
+  for (;;) {
+    seen.add(pos);
+    if (ops[pos].op === 'acc') {
+      acc += ops[pos].val;
+    }
+    pos += ops[pos].op === 'jmp' ? ops[pos].val : 1;
+    if (seen.has(pos)) {
+      break;
+    }
+  }
+  console.log(acc);
+}
