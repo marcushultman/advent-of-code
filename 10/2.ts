6e9e164861df16aba5ade6df13eefd684608dfda
Marcus Hultman <marcus.hultman1@gmail.com>
2020-12-12 13:44:45 +0100

    10: improvements

--- 10/2.ts
+++ 10/2.ts
@@ -1,33 +1,19 @@
 import puzzle from '../util/puzzle.ts';
 
-// 220
-
-let numbersa = await puzzle(import.meta).numbers();
-// numbers = new Set(numbers.sort((a, b) => a - b));
-let numbers = new Set(numbersa);
+const numbers = new Set(await puzzle(import.meta).numbers());
+numbers.add(164);
 
 const m = new Map<number, number>();
-m.set(0, 1);
-m.set(1, 1);
-m.set(2, 2);
-m.set(3, 4);
 
-function arr(jolt) {
+function count(jolt) {
   if (m.has(jolt)) {
     return m.get(jolt);
+  } else if (!numbers.has(jolt)) {
+    return Number(!jolt);
   }
-  let a = 0;
-  if (numbers.has(jolt - 3)) {
-    a += (arr(jolt - 3));
-  }
-  if (numbers.has(jolt - 2)) {
-    a += (arr(jolt - 2));
-  }
-  if (numbers.has(jolt - 1)) {
-    a += (arr(jolt - 1));
-  }
+  const a = count(jolt - 3) + count(jolt - 2) + count(jolt - 1);
   m.set(jolt, a);
   return a;
 }
 
-console.log(arr(164))
+console.log(count(164))
