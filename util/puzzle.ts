54d92a0f24393aefc620db3d82e207b0d3694c35
Marcus Hultman <marcus.hultman1@gmail.com>
2024-01-02 09:25:10 -0500

    util: allow custom test file

--- util/puzzle.ts
+++ util/puzzle.ts
@@ -1,26 +1,29 @@
-import * as path from "std/path/mod.ts";
-import { readLines, readStringDelim } from "std/io/mod.ts";
-import { from } from "https://esm.sh/rxjs@7.8.1";
-import { BoundedGrid } from "./grid.ts";
+import * as path from 'std/path/mod.ts';
+import { readLines, readStringDelim } from 'std/io/mod.ts';
+import { from } from 'https://esm.sh/rxjs@7.8.1';
+import { BoundedGrid } from './grid.ts';
 
-export default function puzzle(meta: ImportMeta, test = false) {
-  const file = path.join(path.dirname(new URL(meta.url).pathname), test ? 'test' : 'input');
+export default function puzzle(meta: ImportMeta, test: boolean | string = false) {
+  const file = path.join(
+    path.dirname(new URL(meta.url).pathname),
+    typeof test === 'string' ? test : test ? 'test' : 'input',
+  );
   const string = () => Deno.readTextFile(file);
   const strings = async () => {
     return (await string()).split('\n');
   };
   const numbers = async () => {
-    return (await strings()).map(s => parseInt(s, 10));
+    return (await strings()).map((s) => parseInt(s, 10));
   };
   const sections = async () => {
     return (await string()).split(/\n\n+/);
   };
   const map = async <T>(f: (line: string) => T) => {
     return (await strings()).map(f);
-  }
+  };
   const charGrid = async () => {
     return BoundedGrid.charGridFromLines(await strings());
-  }
+  };
   const rx = {
     lines: () => from(readLines(Deno.openSync(file))),
     numbers: () => from(readLines(Deno.openSync(file))),
